645b1b5d5e563e30cc363d2d4eaa1a99acbe9799
Leonam de Paula <leonamjlpaula@gmail.com>
2023-01-25 22:55:21 -0300

    Removed firebase test config

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { AuthProvider } from '@app/context/AuthProvider';
 import { defaultTheme } from '@app/styles/defaultTheme';
-import firestore from '@react-native-firebase/firestore';
 import { NavigationContainer } from '@react-navigation/native';
 import { DefaultTheme, ThemeProvider } from 'styled-components/native';
 
 import { AppRoutes } from './navigators';
 
 export default function App() {
-    //TODO: Remove
-    useEffect(() => {
-        firestore().settings({
-            persistence: false,
-        });
-    }, []);
-
     return (
         <NavigationContainer>
             <ThemeProvider theme={defaultTheme satisfies DefaultTheme}>
